84919cb4535470f924be7fd1858de776bb878c15
Yui Chun Leung <leo.leung@faculty.ai>
2022-09-03 16:26:47 +0100

    ts types

--- src/ts/macos_install_vscode_test/app.js
+++ src/ts/macos_install_vscode_test/app.js
@@ -1,2 +1,61 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
 console.log("hello world...");
 console.log("bye bye...");
+var number1 = "3";
+var number2 = 2.8;
+var s1 = "hello";
+var b1 = true;
+var n3;
+n3 = 123;
+var result = add(+number1, number2);
+console.log(result);
+console.log(n3, s1, b1);
+// object type
+var person = {
+    name: "Loe",
+    age: 111
+};
+console.log(person.name);
+// array
+var group = ["Leo", "Sam"];
+var groupMix = ["abc", 1];
+for (var _i = 0, group_1 = group; _i < group_1.length; _i++) {
+    var mem = group_1[_i];
+    console.log(mem);
+}
+// tuple
+var tu = [2, "author"]; //array
+var tu2 = [2, "author"]; //tuple: fixed length, fixed type
+// enum
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 1] = "ADMIN";
+    Role[Role["READ_ONLY"] = 2] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 3] = "AUTHOR";
+})(Role || (Role = {}));
+console.log(Role.ADMIN);
+// any type
+var whatever;
+whatever = 2;
+whatever = "a";
+// union type
+var unionTypeVar = 3;
+// literal type
+var literalTypeVar = 3;
+var customTypeVar = false;
+// void vs undefined
+function printResult(num) {
+    console.log(num);
+}
+function returnUndefined() {
+    return;
+}
+printResult(customTypeVar);
+returnUndefined();
+// Function
+var handleFunc;
+handleFunc = add;
+console.log(handleFunc(2, 3));
+// Callback
--- src/ts/macos_install_vscode_test/app.ts
+++ src/ts/macos_install_vscode_test/app.ts
@@ -1,2 +1,82 @@
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
 console.log("hello world...");
-console.log("bye bye...")
+console.log("bye bye...");
+
+const number1 = "3";
+const number2 = 2.8;
+let s1: string = "hello";
+let b1: boolean = true;
+let n3: number;
+n3 = 123;
+
+const result = add(+number1, number2);
+console.log(result);
+console.log(n3, s1, b1);
+
+// object type
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Loe",
+  age: 111,
+};
+
+console.log(person.name);
+
+// array
+const group: string[] = ["Leo", "Sam"];
+const groupMix: any[] = ["abc", 1];
+for (const mem of group) {
+  console.log(mem);
+}
+
+// tuple
+const tu: (string | number)[] = [2, "author"]; //array
+const tu2: [number, string] = [2, "author"]; //tuple: fixed length, fixed type
+
+// enum
+enum Role {
+  ADMIN = 1,
+  READ_ONLY,
+  AUTHOR,
+}
+console.log(Role.ADMIN);
+
+// any type
+let whatever: any;
+whatever = 2;
+whatever = "a";
+
+// union type
+let unionTypeVar: number | string = 3;
+
+// literal type
+let literalTypeVar: 1 | 3 = 3;
+
+// custom type
+type Custom = number | boolean;
+const customTypeVar: Custom = false;
+
+// void vs undefined
+function printResult(num: Custom): void {
+  console.log(num);
+}
+
+function returnUndefined(): undefined {
+  return;
+}
+
+printResult(customTypeVar);
+
+returnUndefined();
+
+// Function
+let handleFunc: (a: number, b: number) => number;
+handleFunc = add;
+console.log(handleFunc(2,3));
+
+// Callback
