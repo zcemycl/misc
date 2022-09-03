4fa2dfd36ded5e946d0679690a32c04a3eeac7fd
Yui Chun Leung <leo.leung@faculty.ai>
2022-09-03 09:40:00 +0100

    update

--- src/cpp/macos_install_vscode_test/main.cpp
+++ src/cpp/macos_install_vscode_test/main.cpp
@@ -1,7 +1,28 @@
+/*
+author: Leo
+date: ??/??/????
+*/
 #include <iostream>
 using namespace std;
+using std::cout;
+using std::cin;
+using std::endl; 
 
-int main() {
+int age {18};
+
+int main(int argc, char *argv[]) {
+    // comment me
+    int a = 133;
+    cout << a << endl;
+    float b = 1.2;
+    /*
+    Life is tired
+    */
+    cout << b << endl;
+    int* dyn = new int;
+    *dyn = 134444;
+    cout << *dyn << endl;
+    delete dyn;
     cout << "Hello World!!" << endl;
     return 0;
 }
