de2f010e989b0025d99955c4e2cee595d7526d9f
Yui Chun Leung <leo.leung@faculty.ai>
2022-09-07 16:01:41 +0100

    update

--- src/ts/macos_install_vscode_test/src/analytics.ts
+++ src/ts/macos_install_vscode_test/src/analytics.ts
@@ -7,3 +7,34 @@ button.addEventListener("click", () => {
   a += 1;
   console.log(a);
 });
+
+class Person {
+  name: string;
+  private bills: string[] = [];
+  readonly id: string = '123';
+
+  constructor(n: string){
+    this.name = n;
+  }
+
+  introduce(this: Person) {
+    console.log("My name is: ", this.name);
+  } 
+
+  addBill(billName: string) {
+    this.bills.push(billName);
+  }
+
+  printBills() {
+    console.log(this.bills);
+  }
+
+}
+
+const leo = new Person('Leo');
+leo.introduce();
+leo.addBill('water');
+leo.addBill('energy');
+leo.printBills();
+console.log(leo.id);
+console.log(leo)
