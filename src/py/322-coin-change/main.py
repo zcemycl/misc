4f8802e4f0ef31de554344163569c3e5f6057984
Yui Chun Leung <leo.leung@faculty.ai>
2023-03-19 17:17:05 +0000

    update

--- src/py/322-coin-change/main.py
+++ src/py/322-coin-change/main.py
@@ -22,11 +22,31 @@ class Solution:
         dfs(0, 0, amount, [])
         return cls.result if cls.result!=10**5 else -1
 
+    @classmethod
+    def dp(cls, coins, amount):
+        if amount == 0:
+            return 0
+        dps = [amount+1 for i in range(amount+1)]
+        dps[0] = 0
+        for i in range(1, amount+1):
+            if i in coins:
+                dps[i] = 1
+            else:
+                for c in coins:
+                    if c > i or i < c:
+                        continue
+                    dps[i] = min(dps[i], 1+dps[i-c])
+        print(dps)
+        return dps[-1] if dps[-1]!=amount+1 else -1
+
 if __name__ == "__main__":
     inputs = [
         ([1,2,5],11,3),
         ([2],3,-1),
-        ([1],0,0)
+        ([1],0,0),
+        ([2,5,10],25,3),
+        ([3,7,405,436],8839,25)
     ]
     for coins, amount, result in inputs:
-        print(Solution.naive_solution(coins, amount))
+        # print(Solution.naive_solution(coins, amount))
+        print(Solution.dp(coins, amount))
