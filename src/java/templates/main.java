68314b5f9ed81a7eff86c8881ffe5060c324c8ab
Yui Chun Leung <leo.leung@faculty.ai>
2023-03-21 17:09:17 +0000

    update

---
+++ src/java/templates/main.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int random = 5;
+    private static int random2 = 10;
+    int modelYear;
+
+    public Solution(int year) {
+        modelYear = year;
+    }
+
+    public void naive() {
+        System.out.println("Hello World!");
+    }
+
+    public static void naive2() {
+        System.out.println("Hello World2!");
+    }
+
+    public static void naive3() {
+        // System.out.println(random); // not accessible
+        System.out.println(random2);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution(2020);
+        s.naive();
+        naive2();
+        naive3();
+        System.out.println(s.random);
+    }
+}
