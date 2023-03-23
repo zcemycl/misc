820e66d016f50a6956c0b61b7f4a34d1045f36ab
Yui Chun Leung <leo.leung@faculty.ai>
2023-03-23 09:57:30 +0000

    update

---
+++ src/java/226-invert-binary-tree/main.java
@@ -0,0 +1,12 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
