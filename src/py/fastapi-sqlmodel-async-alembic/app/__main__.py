cebefe93908caa5cef0f9cbfdbec8e870a8e1347
zcemycl <lyc010197@gmail.com>
2023-07-10 22:37:59 +0100

    update

--- src/py/fastapi-sqlmodel-async-alembic/app/__main__.py
+++ src/py/fastapi-sqlmodel-async-alembic/app/__main__.py
@@ -1,3 +1,28 @@
-from app import settings
+import uvicorn
+from fastapi import FastAPI
 
-print(settings)
+from app import settings
+from app.core.models import HealthCheck
+from loguru import logger
+
+
+logger.info(f"settings debug: {settings.debug}")
+
+app = FastAPI(
+    title = settings.project_name,
+    version = settings.version,
+    openapi_uri = f"{settings.api_v1_prefix}/openapi.json",
+    debug = settings.debug
+)
+
+@app.get("/", response_model=HealthCheck, tags=["status"])
+async def health_check():
+    return {
+        "name": settings.project_name,
+        "version": settings.version,
+        "description": settings.description
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8080, host="0.0.0.0", reload=True)
